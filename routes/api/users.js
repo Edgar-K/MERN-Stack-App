fd8fee225d6c82cc9c3b1cfe041fbbd7a720fd86
Edgar Kocharyan <58892166+Edgar-K@users.noreply.github.com>
2020-07-16 23:59:43 +0200

    User Registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -28,13 +28,13 @@ router.post(
             return res.status(400).json({ errors: errors.array() });
         }
 
-        const { name, email, password } = req.body;
+        const {name, email, password}= req.body;
 
         try {
             //See if user exist
-            let user = await User - findOne({ email });
+            let user = await User.findOne({ email });
             if (user) {
-               return res.status(400).json({ errors: [{ msg: 'User already exist' }] });
+                return res.status(400).json({ errors: [{ msg: 'User already exist' }] });
             }
 
             // Get users gravatar
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const connectDB = require('./config/db');
 
+
 const app = express();
 
 // Connect Database
@@ -8,15 +9,18 @@ const app = express();
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false }));
+app.use(express.json({ extended: true }));
+
+
+
 
 app.get('/', (req, res) => res.send('Api Running'));
 
 // Define Routes
-app.use('api/users', require('./routes/api/users'));
-app.use('api/auth', require('./routes/api/auth'));
-app.use('api/profile', require('./routes/api/profile'));
-app.use('api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 
 const PORT = process.env.PORT || 5000;
